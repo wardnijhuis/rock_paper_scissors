d6a3bd77eae2da325c7a112d78d7d3b57f5d1733
WardTheProgrammer <nijhuis.ward@gmail.com>
2024-10-30 12:00:24 +0100

    Add a function get the user choice via a prompt

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+const choices = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
 
     const randNum = Math.floor(Math.random() * 3);
 
     return choices[randNum];
 }
 
+function getHumanChoice(error) {
+    const input = (error ? prompt("Your input was incorrect, try again:") : prompt("Make your choice wisely:")).toLowerCase();
+
+    if (choices.includes(input)) {
+        return input;
+    }
+    return getHumanChoice(true);
+}
+
 console.log(getComputerChoice())
+
+console.log(getHumanChoice(false))
