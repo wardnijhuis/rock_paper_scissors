19379f02634ef962c0f09e86e028c5a2954ccb3d
WardTheProgrammer <nijhuis.ward@gmail.com>
2024-10-30 12:22:44 +0100

    Add a playRound function to determine the winner of one round

--- script.js
+++ script.js
@@ -8,7 +8,13 @@ function getComputerChoice() {
 }
 
 function getHumanChoice(error) {
-    const input = (error ? prompt("Your input was incorrect, try again:") : prompt("Make your choice wisely:")).toLowerCase();
+    let input = error ? prompt("Your input was incorrect, try again:") : prompt("Make your choice wisely:");
+
+    if (input === null) {
+        return getHumanChoice(false);
+    }
+    
+    input = input.toLowerCase();
 
     if (choices.includes(input)) {
         return input;
@@ -17,3 +23,64 @@ function getHumanChoice(error) {
 }
 
 let humanScore = 0, computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    let winner;
+
+    switch (humanChoice) {
+        case "rock":
+            switch (computerChoice) {
+                case "rock":
+                    winner = "tie";
+                    break;
+                case "paper":
+                    winner = "computer";  
+                    break;
+                case "scissors":
+                    winner = "human";
+                    break;
+            }
+            break;
+            case "paper":
+                switch (computerChoice) {
+                    case "rock":
+                        winner = "human";
+                        break;
+                    case "paper":
+                        winner = "tie";  
+                        break;
+                    case "scissors":
+                        winner = "computer";
+                        break;
+                }
+                break;
+            case "scissors":
+                switch (computerChoice) {
+                    case "rock":
+                        winner = "computer";
+                        break;
+                    case "paper":
+                        winner = "human";  
+                        break;
+                    case "scissors":
+                        winner = "tie";
+                        break;
+                    }
+                    break;
+    }
+
+    switch (winner) {
+        case "human":
+            humanScore++;
+            console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+            return "human";
+        case "tie":
+            console.log(`It's a tie! Both of you chose ${humanChoice}.`);
+            return "tie";
+        case "computer":
+            computerScore++;
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+            return "computer";
+    }
+}
+
