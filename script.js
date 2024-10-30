24c7a8da4d4bb29bb8ccd85bec916b4ce779289e
WardTheProgrammer <nijhuis.ward@gmail.com>
2024-10-30 11:42:39 +0100

    Add a function to get a random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+
+    const randNum = Math.floor(Math.random() * 3);
+
+    return choices[randNum];
+}
+
+console.log(getComputerChoice())
